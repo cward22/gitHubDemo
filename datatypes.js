ee8f8b9abfcbf5f24e2ed778a83156d1b44c2733
cward22 <h2hworldwide@gmail.com>
2020-01-30 13:00:35 -0500

    this should work

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// #1 - Adding 2 Strings Together
+    console.log("2" + "2");
+
+// #1 -Output Returned: 22
+
+// #2 - Declaring Variables
+var str1; 
+var str2;
+
+// #2 - Assigning Values to Variables
+str1 = "Today's ";
+str2 = "a good day.";
+
+// #2 - Output Returned: Today's a good day.
+    console.log(str1 + str2);
+
+// #3 - Declaring Variables
+var music; 
+var volume;
+
+// #3 - Assigning Values to Variables
+music = "loud";
+volume = 7;
+
+// #3 - Output returned: NaN
+    console.log(music * volume);
+
+// Just tried something to see the answer; got NaN
+    console.log(music * volume + 2);
+    console.log(music * volume + 2 + true);
+
+// #4 - Bonus returned: 3
+    console.log(2 + true);
