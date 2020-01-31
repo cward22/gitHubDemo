e529fcc6cecc416fa4714058c80c405878e1393b
cward22 <h2hworldwide@gmail.com>
2020-01-31 09:59:28 -0500

    added notes

--- datatypes.js
+++ datatypes.js
@@ -150,7 +150,10 @@ var skip
     console.log ('Good Morning!'); // I will invoke myself
 })();
 
-var len =str.length;
+// Notes - 1/31/20
+
+var str = "I'm 25";
+var len = str.length;
 console.log(len);
 console.log(str[0]);
 console.log(str[10]);
@@ -158,3 +161,13 @@ console.log(str[10]);
 for(var i = 0; i < str.length; i++) {
         console.log(str[i]);
 }
+
+var str = "hello world";
+
+for(var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
+
+var upperCaseStr = str.toUpperCase();
+console.log(upperCaseStr);
+console.log(upperCaseStr.toLowerCase());
