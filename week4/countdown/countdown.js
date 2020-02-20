be98891579647801e9761f652f5a5f2cae552e31
cward22 <h2hworldwide@gmail.com>
2020-02-20 14:28:13 -0500

    created an input box for countdown timer

---
+++ week4/countdown/countdown.js
@@ -0,0 +1,50 @@
+// var i = 11;
+
+// (function countDown(){
+//     if (--i < 0) return;
+//     setTimeout(function(){
+//         console.log (i + "seconds");
+//     countDown();
+//     }, 1000);
+// })();
+
+// Define variable
+
+// var timer = 11;
+
+// // Create a function
+
+// function countDown(){
+//     for (var timer = 11 ; timer > 0; timer--) {
+//         //log timer to the console
+//         console.log (timer);
+//     }
+// }
+
+// Define variable
+
+// var timer = 8;
+
+// // Create a function
+
+// function countDown(){
+//     while(timer > 0){
+//         console.log(getElementById("paragraph").innerHTML);
+//         timer--;
+//     }
+// }
+
+// Define a variable
+
+var timer = 8;
+
+// Create a function
+
+// (function countDown(){
+//     if (--timer < 0) return;
+//     setTimeout(function(){
+//         document.getElementById("number")[0].innerHMTL = i + " seconds";
+//         countDown();
+//     },  1000);
+// })();
+
