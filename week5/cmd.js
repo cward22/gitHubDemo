e2549b512dde4370538e848c3b8604ec2b0e69ed
cward22 <h2hworldwide@gmail.com>
2020-02-24 15:01:49 -0500

    added cmd and wait excercise files

---
+++ week5/cmd.js
@@ -0,0 +1,5 @@
+// console.log("Hello World");
+const app2 = require('./cmd2.js');
+
+app2.add(5,5);
+app2.sub(10,5);
---
+++ week5/cmd2.js
@@ -0,0 +1,12 @@
+const add = function (x,y) {
+    console.log(x + y);
+}
+
+const sub = function (x,y) {
+    console.log(x - y);
+}
+
+module.exports = {
+    add: add,
+    sub: sub
+}
