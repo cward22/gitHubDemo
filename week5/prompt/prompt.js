39a5445136740617281167734eb3a3ecdc7d9a22
cward22 <h2hworldwide@gmail.com>
2020-02-26 11:58:24 -0500

    commented out scratchwork

--- week5/prompt/prompt.js
+++ week5/prompt/prompt.js
@@ -1,9 +1,38 @@
+// Created a Node Prompt that asks a question 
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
 readline.question(`What's your first name?`, (firstName) => {
-    console.log(`Hola ${firstName}!`)
-    readline.close
+    console.log(`Hola ${firstName}!`)   
+            readline.close
 })
+
+// const readline2 = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+
+// readline2.question(`What is your favorite song?`, (favoriteSong) => {
+//     console.log(`${favoriteSong} is a great song!`)
+//     readline2.close
+// })
+
+
+// const inquirer = require('inquirer')
+
+// var questions = [{
+//     type: 'input',
+//     name: 'firstName',
+//     message: "What is your first name?",
+//     type: 'input',
+//     song: 'favoriteSong',
+//     message: "What is the name of your favorite song?",
+// }]
+
+// inquirer.prompt(questions).then(answers => {
+//     console.log(`Hola ${firstName}!`)
+//     console.log(`${favoriteSong} is a great song!`)
+// })
