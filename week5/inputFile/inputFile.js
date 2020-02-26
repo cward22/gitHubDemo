8f5b5d14efaa22176c5a01997a69e81ca52b1989
cward22 <h2hworldwide@gmail.com>
2020-02-26 15:57:59 -0500

    added appendFile function

--- week5/inputFile/inputFile.js
+++ week5/inputFile/inputFile.js
@@ -1,17 +1,25 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
 // const readline = require('readline');
-const fs = require('fs');
+// const fs = require('fs');
 
-readline.question(`What is your favorite number?`, (number) => {
-    console.log(`Awesome!`)   
-    readline.close
-})
+// readline.question(`What is your favorite number?`, (number) => {
+//     console.log(`Awesome!`)   
+//     readline.close
+//         fs.appendFile('newfile.txt', '(`${number}`)', 
+//         function (err) {
+//             if (err) throw err;
+//             console.log("The number has been added to the file!");
+//     });
+// })
 
-fs.appendFile('newfile.txt', '(`${number}`)', 
-function (err) {
+var fs = require('fs');
+
+// appendFile with new input/text
+
+fs.appendFile("newfile.txt", "First thing's first", function (err) {
     if (err) throw err;
-    // console.log("Added!");
+    console.log("File has been saved.");
 });
