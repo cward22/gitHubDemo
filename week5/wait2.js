cc8afc972930b23a7c350d1c7002fb8c681b47e2
cward22 <h2hworldwide@gmail.com>
2020-02-26 08:45:39 -0500

    removed commented out promise scratch work

--- week5/wait2.js
+++ week5/wait2.js
@@ -5,32 +5,3 @@ const greeting = function myFunction2() {
 }
 
 module.exports = greeting
-
-// function myFunction() {
-//     setInterval(function() {
-//         console.log("What is up?");
-//     }, 10000);
-// }
-
-// var promise = new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//         console.log('completed');
-//         resolve({data: '123'});
-//     }, 12000);
-// });
-// return promise;
-
-// myFunction().then(promise);
-
-
-// const promise = new Promise(function(resolve, reject) {
-//     resolve('Success');
-// });
-
-// setTimeout(() => {
-//     console.log(then)
-// }
-
-// promise.then(function(value) {
-//     console.log(value);
-// });
