01741e33fb0ced022ebfb44bd98c7f2edc3352a3
cward22 <h2hworldwide@gmail.com>
2020-02-27 12:00:52 -0500

    added /songs to server

--- week5/server/firstServer.js
+++ week5/server/firstServer.js
@@ -2,11 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     if (req.url === '/') {
-        res.write('Hola world!');
+        res.write('Hola, World!');
+        res.end();
+    }
+
+    if (req.url === '/songs') {
+        res.write(JSON.stringify(["Remember", "We Dance", "Evergreen"]));
         res.end();
     }
 });
-
 
 // server.on('connection', socket => {
 //     console.log("connected");
