6737c7f53bfa547d96c7107f65f5657c577e5b21
cward22 <h2hworldwide@gmail.com>
2020-02-27 11:29:50 -0500

    I got the number input by a user to show up in the txt file

--- week5/inputFile/inputAppend.js
+++ week5/inputFile/inputAppend.js
@@ -15,11 +15,27 @@
 //     });
 // })
 
-var fs = require('fs');
+// var fs = require('fs');
 
-// appendFile with new input/text
+// // appendFile with new input/text
 
-fs.appendFile("newfile.txt", "First thing's first", function (err) {
-    if (err) throw err;
-    console.log("File has been saved.");
-});
+// fs.appendFile("newfile.txt", "First thing's first", function (err) {
+//     if (err) throw err;
+//     console.log("File has been saved.");
+// });
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+const fs = require('fs');
+
+readline.question(`What is your favorite number?`, (number) => {
+    console.log(`Awesome!`)   
+    readline.close
+        fs.appendFile('newfile.txt', (number), 
+        function (err) {
+            if (err) throw err;
+            console.log("The number has been added to the file!");
+    });
+})
