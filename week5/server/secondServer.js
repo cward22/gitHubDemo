0dd2e0ca04fc907ce7c7aad020be70d4ee8339ab
cward22 <h2hworldwide@gmail.com>
2020-02-28 10:39:39 -0500

    created server that navigates to /about page

---
+++ week5/server/secondServer.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const greeting = "Hola, World!";
+const heading = "About Me Page"
+
+//create a server object
+const server = http.createServer((req, res)=>{
+    if (req.url === '/') {
+        res.write(`<h1>${greeting}</h1>`);
+        res.end(); // ends the respone
+    }
+
+// Make it a web server so that when a browser navigates to "localhost:8080/about" you server a html web page.
+
+    if (req.url === '/about') {
+        res.write(heading);
+        res.end(); // ends the respone
+    }
+});
+
+//the server obeject is listening on port 8080
+server.listen(8080);
+console.log("server is on");
